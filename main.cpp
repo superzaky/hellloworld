35d7df828b7866db8f44a89e806b1adb4d157339
Yomacho <yomacho@kali.yomacho.org>
2023-07-21 00:54:51 +0200

    app integration with library

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+// #include "/home/yomacho/Documents/importer/Structure.hpp"
+#include "Structure.hpp"
+
+int main() {
+    int a = 5;
+    int b = 8;
+    int c = sum( a, b );
+
+    printf( "sum of %d and %d is %d\n", a, b, c );
+
+    return 0;
+}
